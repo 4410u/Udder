856f6db557a8873884612e25805d5a464f5f4741
Sliver <yosi_4410u@outlook.jp>
2022-10-28 20:06:15 +0900

    Add label.

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(.systemBlue)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Udder")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(.white)
+                Spacer()
+                Text("Fresh Milk, On Demand.")
+                Spacer()
+                    .padding(.top)
+            }
+            
+            .padding()
         }
-        .padding()
+        
     }
 }
 
