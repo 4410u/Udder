74d3c7258bacac34af30fd0cee35e62ec2648eb8
Sliver <yosi_4410u@outlook.jp>
2022-10-28 19:58:51 +0900

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Sliver ㅤ‮ㅤ‮ on 28/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
